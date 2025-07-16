00ba05b774ada9929b08b66abbf87fff231560d1
Gabriel Montero <gasamoma@amazon.com>
2025-07-16 09:22:57 -0500

    allowing the stack to reference nested stack

--- source/cdk/app.py
+++ source/cdk/app.py
@@ -13,13 +13,15 @@ waf_stack = WafStack(app, "WafStack",
     env=cdk.Environment(
         account=os.environ["CDK_DEFAULT_ACCOUNT"],
         region="us-east-1"),
-    description="WAF for CloudFront in us-east-1")
+    cross_region_references=True,
+    description="Guidance for Low Cost Semantic search on AWS (5179), (SO9030) WAF in us-east-1")
 
 # Deploy main stack in the default region
 main_stack = ChatbotStack(app, "ChatbotStack",
     env=cdk.Environment(
         account=os.environ["CDK_DEFAULT_ACCOUNT"],
         region=os.environ["CDK_DEFAULT_REGION"]),
+    cross_region_references=True,
     waf_acl_arn=waf_stack.waf_acl_arn,
     description="Guidance for Low Cost Semantic search on AWS (5179), (SO9030)")
 
