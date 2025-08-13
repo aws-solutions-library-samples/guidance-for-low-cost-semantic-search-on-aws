892a8840f534087b22af3ecf3a501e617bb3421d
Gabriel Montero M <gasamoma@amazon.com>
2025-08-13 11:56:56 -0600

    Update chatbot_stack.py

--- source/cdk/chatbot/chatbot_stack.py
+++ source/cdk/chatbot/chatbot_stack.py
@@ -993,9 +993,9 @@ class ChatbotStack(Stack):
             iam.PolicyStatement(
                 actions=["bedrock:InvokeModel", "ssm:GetParameter"],
                 resources=[
-                    "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude*",
+                    "arn:aws:bedrock:*::foundation-model/anthropic.claude*",
                     # arn:aws:bedrock:us-east-1::foundation-model/amazon.titan-embed-text-v1 
-                    "arn:aws:bedrock:us-east-1::foundation-model/amazon.titan-embed-text-v2:0",
+                    "arn:aws:bedrock:*::foundation-model/amazon.titan-embed-text-v2:0",
                     "arn:aws:ssm:*:*:parameter/aibot_promt_system",
                     "arn:aws:ssm:*:*:parameter/aibot_promt_context"
                 ]
